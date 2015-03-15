78d8af572f799d62064144534d915adccf422325
jcgsxr <johnnyjchan@gmail.com>
2015-03-15 01:36:08 -0700

    Gopher now hides since adjusting image height is not feasible on the watch

--- GopherSmash WatchKit Extension/InterfaceController.m
+++ GopherSmash WatchKit Extension/InterfaceController.m
@@ -11,18 +11,21 @@
 
 @interface InterfaceController()
 
-@property (weak, nonatomic) IBOutlet WKInterfaceImage *spacerImage;
 @property (weak, nonatomic) IBOutlet WKInterfaceImage *gopherImage;
-@property (assign, nonatomic) CGFloat origImageHeight;
-@property (assign, nonatomic) CGFloat gopherHeight;
 @property (strong, nonatomic) NSTimer *timer;
+@property (assign, nonatomic) NSUInteger level;
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel *levelLabel;
+@property (weak, nonatomic) IBOutlet WKInterfaceButton *smashButton;
+@property (assign, nonatomic) BOOL isImageHidden;
 
 @end
 
-static CGFloat kMinGopherHeight = 0.1f;
-static CGFloat kGrowHeight = 2;
-static CGFloat kMaxGopherHeight = 150;
-static CGFloat kGopherTime = 1;
+
+
+
+static CGFloat kGopherTime = 1.0f;
+static NSString *kLevelKey = @"level";
+static NSString *kSuiteName = @"com.llamaface.gophersmash";
 
 @implementation InterfaceController
 
@@ -30,15 +33,15 @@ static CGFloat kGopherTime = 1;
     [super awakeWithContext:context];
 
     // Configure interface objects here.
+	self.isImageHidden = NO;
 	
-	self.origImageHeight = kMaxGopherHeight;
-	self.gopherHeight = kMinGopherHeight;
-	[self.gopherImage setHeight:kMinGopherHeight];
-	self.timer = [NSTimer scheduledTimerWithTimeInterval:kGopherTime
-												  target:self
-												selector:@selector(gopherTimer:)
-												userInfo:nil
-												 repeats:YES];
+	NSUserDefaults *defaults = [[NSUserDefaults alloc] initWithSuiteName:kSuiteName];
+	self.level = [defaults integerForKey:kLevelKey];
+	
+	if (self.level <= 0)
+		self.level = 1;
+
+	[self beginTimer];
 }
 
 - (void)willActivate {
@@ -53,30 +56,64 @@ static CGFloat kGopherTime = 1;
     [super didDeactivate];
 }
 
-- (void)gopherTimer:(NSTimer*)timer
-{
+- (void)gopherTimer:(NSTimer*)timer {
 //	NSLog(@"gopherTimer");
 	
-	self.gopherHeight -= kGrowHeight;
+	self.isImageHidden = YES;
+	[self.gopherImage setHidden:self.isImageHidden];
+
+	double randSec = (arc4random() % 5);
+	if (randSec < 2)
+		randSec = 2;
 	
-	[self verifyGopherHeight];
-	
-	[self.spacerImage setHeight:self.gopherHeight];
+	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(randSec * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+		[self beginTimer];
+	});
 }
 
-- (void)verifyGopherHeight
+- (void)beginTimer
 {
-	self.gopherHeight = (self.gopherHeight >= kMaxGopherHeight) ? kMaxGopherHeight : self.gopherHeight;
+	NSLog(@"beginTimer");
+	self.isImageHidden = NO;
+	[self.gopherImage setHidden:self.isImageHidden];
 	
-	self.gopherHeight = (self.gopherHeight < kMinGopherHeight) ? kMinGopherHeight : self.gopherHeight;
+	CGFloat levelTime = (CGFloat)(self.level * 2) / 100;
+	self.timer = [NSTimer scheduledTimerWithTimeInterval:kGopherTime - levelTime
+												  target:self
+												selector:@selector(gopherTimer:)
+												userInfo:nil
+												 repeats:NO];
 }
 
 - (IBAction)smashAction {
-	self.gopherHeight += kGrowHeight;
+	[self.smashButton setEnabled:NO];
+	[self.timer invalidate];
+	[self.gopherImage setHidden:YES];
 	
-	[self verifyGopherHeight];
-	
-	[self.spacerImage setHeight:self.gopherHeight];
+	// TODO: Figure out actual value of spacer height that indicates that the gopher image is visible.
+	if (!self.isImageHidden) {
+		[self.levelLabel setText:@"Nice job! Get ready..."];
+		
+		// Proceed to next level.
+		self.level++;
+		NSUserDefaults *defaults = [[NSUserDefaults alloc] initWithSuiteName:kSuiteName];
+		[defaults setInteger:self.level forKey:kLevelKey];
+
+		dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+			[self.smashButton setEnabled:YES];
+			[self.levelLabel setText:[NSString stringWithFormat:@"Level: %ld", self.level]];
+			[self beginTimer];
+		});
+	}
+	else {
+		[self.levelLabel setText:@"You missed! Get ready..."];
+		
+		dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+			[self.smashButton setEnabled:YES];
+			[self.levelLabel setText:[NSString stringWithFormat:@"Level: %ld", self.level]];
+			[self beginTimer];
+		});
+	}
 }
 
 @end
